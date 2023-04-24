ab593cc0c8be211bb703cd9269a0aebefa8406e3
YaroslavFominEleks <yaroslav.fomin@eleks.com>
2023-04-24 13:46:17 +0300

    update eslint & tseslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,48 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
   extends: [
     'next/core-web-vitals',
-    'eslint-config-prettier'
-  ]
-}
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'eslint-config-prettier',
+  ],
+  ignorePatterns: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+  rules: {
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        // interface should not start with "I" (e.g ISomething)
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+      },
+    ],
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: [
+          'public-field',
+          'protected-field',
+          'private-field',
+
+          'public-method',
+          'protected-method',
+          'private-method',
+        ],
+      },
+    ],
+    camelcase: ['error'],
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'newline-after-var': 'error',
+  },
+  
+};
