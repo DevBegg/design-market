55c2dc325e15d5d08277c9afd02e34821d16349b
YaroslavFominEleks <yaroslav.fomin@eleks.com>
2023-04-24 16:21:36 +0300

    some default mixins and vars scss

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import styles from './header.module.scss';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerWrapper}>some content</div>
+    </header>
+  );
+};
