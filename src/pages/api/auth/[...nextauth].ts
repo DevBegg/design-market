db7c4a2d42d03aeb20d02c172130a941ce0f5742
DevBegg <yaroslavfomink@gmail.com>
2023-05-27 19:49:48 +0300

    secret & some improvements

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { CallbacksOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
@@ -8,9 +8,28 @@ export const authOptions = {
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET as string,
     }),
   ],
+  callbacks: {
+    async signIn({ account, ...signInProps }): Promise<boolean> {
+      if (account?.provider === 'google') {
+        const { user } = signInProps;
+
+        const googleUser = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        };
+
+        console.log('googleUser', googleUser);
+        // user.accessToken = await getTokenFromYourAPIServer('google', googleUser)
+        return true;
+      }
+      return false;
+    },
+  } as Partial<CallbacksOptions>,
   //https://next-auth.js.org/getting-started/upgrade-v4#missing-secret
   //generate secret
-  secret: 'SECRET',
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
